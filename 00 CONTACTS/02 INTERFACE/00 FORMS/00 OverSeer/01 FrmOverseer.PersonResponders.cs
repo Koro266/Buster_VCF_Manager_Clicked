3c3cf991907e7e3adb359746128a470e3e54a26b
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-06-25 16:46:37 +1200

    FrmOverseer: Bringing export functions into line with dynamic notifications.

--- 00 CONTACTS/02 INTERFACE/00 FORMS/00 OverSeer/01 FrmOverseer.PersonResponders.cs
+++ 00 CONTACTS/02 INTERFACE/00 FORMS/00 OverSeer/01 FrmOverseer.PersonResponders.cs
@@ -1,14 +1,14 @@
 ﻿//___________________________________________________________________________________________________________________________________________________
 //GLOBAL
+using CONTACTS.GLOBAL.DATABASE.COLUMN;
 using BASE_ROW		= CONTACTS.GLOBAL.DATABASE.ROW.BaseRow;
+using DATE_TIME		= CONTACTS.GLOBAL.DATABASE.COLUMN.Date_Time;
 using GLOBAL_PRESET	= CONTACTS.GLOBAL.VALUES.CONSTANT.Preset;
-using INT_32		= CONTACTS.GLOBAL.DATABASE.COLUMN.Integer_32;
-using SHORT_TEXT	= CONTACTS.GLOBAL.DATABASE.COLUMN.Short_Text;
+using PERSON_COUNT	= CONTACTS.LOCAL.PRIMARY.PERSON.Database.Count;
+using PERSON_FORM	= CONTACTS.INTERFACE.FORMS.FrmPerson;
 //LOCAL
 using PERSON_ROW	= CONTACTS.LOCAL.PRIMARY.PERSON.Row;
 using PERSON_SELECT	= CONTACTS.LOCAL.PRIMARY.PERSON.Database.Select;
-using PERSON_COUNT	= CONTACTS.LOCAL.PRIMARY.PERSON.Database.Count;
-using PERSON_FORM	= CONTACTS.INTERFACE.FORMS.FrmPerson;
 
 //___________________________________________________________________________________________________________________________________________________
 namespace CONTACTS.INTERFACE.FORMS
@@ -60,35 +60,62 @@ namespace CONTACTS.INTERFACE.FORMS
 		/// </summary>
 		private void ExportRecentPersons( DateTime dt )
 		{
+			string msg;
+			DATE_TIME date_time =new DATE_TIME(dt);
+
 			int count = new PERSON_COUNT.CountAfterCurrencyDate( dt ).Execute;
 
-			if ( count <= GLOBAL_PRESET.ZERO )
+			if ( count > GLOBAL_PRESET.ZERO )
 			{
-				_Messenger.Message = $"There are no persons updated after {dt.ToString()}.";
-				return;
+				Dictionary<int, BASE_ROW> base_rows = new PERSON_SELECT.SelectAfterCurrencyDate( dt ).Execute;
+				foreach ( BASE_ROW base_row in base_rows.Values )
+				{
+					PERSON_ROW person_row = ( PERSON_ROW )base_row;
+					person_row.ExportPerson();
+				}
+				msg = $"{count.ToString()} persons updated after {date_time.AsDisplayedDate} have been exported.";
+			}
+			else
+			{
+				msg = $"There are no persons updated after {date_time.As_d_MMM_yyyy}.";
 			}
 
-			Dictionary<int, BASE_ROW> base_rows = new PERSON_SELECT.SelectAfterCurrencyDate( dt ).Execute;
-			foreach ( BASE_ROW base_row in base_rows.Values )
-			{
-				PERSON_ROW person_row = ( PERSON_ROW )base_row;
-				person_row.ExportPerson();
-			}
-			_Messenger.Message = $"There are {count.ToString()} persons updated after {dt.ToString()}.";
-
+			_Messenger.Message = msg;
 		}
 		//___________________________________________________________________________________________________________________________________________
 		/// <summary>
 		/// Processes all Person rows into VCF files.
 		/// </summary>
-		private void ExportAllPersons()
+		private async void ExportAllPersons()
 		{
+			_btn_Export_All_Persons.Enabled = false;
+
 			Dictionary<int, BASE_ROW> base_rows = new PERSON_SELECT.AllPersons().Execute;
-			foreach ( BASE_ROW base_row in base_rows.Values )
+
+			int person_count = base_rows.Count;
+			tbx_Export_Status.Text = $"Working on 0 of {person_count} ...";
+
+			var progress = new Progress<int>( count =>
 			{
-				PERSON_ROW person_row = ( PERSON_ROW )base_row;
-				person_row.ExportPerson();
-			}
+				tbx_Export_Status.Text = $"Working on {count} of {person_count} ...";
+			} );
+
+			await Task.Run( () =>
+			{
+				int current_count = 0;
+				foreach ( BASE_ROW base_row in base_rows.Values )
+				{
+					PERSON_ROW person_row = ( PERSON_ROW )base_row;
+					person_row.ExportPerson();
+					current_count++;
+
+					( ( IProgress<int> )progress ).Report( current_count );
+				}
+			} );
+
+			tbx_Export_Status.Text = "Done!";
+
+			_btn_Export_All_Persons.Enabled = true;
 		}
 	}
 }
