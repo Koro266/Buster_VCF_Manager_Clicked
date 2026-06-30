b423a6aa04ffa1b768c637d3a11405d086797562
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-07-01 04:31:47 +1200

    Inserts an incomplete but cooperative long int (32 bit) field in Person.

---
+++ 00 CONTACTS/01 LOCAL/01 PRIMARY/00 PERSON/03 COLUMNS/18 LI_EHS_Order.cs
@@ -0,0 +1,106 @@
+﻿//___________________________________________________________________________________________________________________________________________________
+using System.Data.OleDb;
+//GLOBAL
+using INT_32	= CONTACTS.GLOBAL.DATABASE.COLUMN.Integer_32;
+using NULL_INT	= CONTACTS.GLOBAL.DATABASE.COLUMN.TypeNullPair<int>;
+using FACTORS	= CONTACTS.GLOBAL.TOOLS.ColumnFactors;
+//LOCAL
+using CONST		= CONTACTS.LOCAL.PRIMARY.PERSON.Constants;
+using ORDINAL	= CONTACTS.LOCAL.PRIMARY.PERSON.Constants.OrdinalByName;
+
+//___________________________________________________________________________________________________________________________________________________
+namespace CONTACTS.LOCAL.PRIMARY.PERSON
+{
+	//_______________________________________________________________________________________________________________________________________________
+	public partial class Column
+	{
+		//___________________________________________________________________________________________________________________________________________
+		public partial class LI_EHS_Order : INT_32
+		{
+			#region DECLARATIONS
+			private static FACTORS column_factors = CONST.Factors[ORDINAL.EHS_Order];
+			private NULL_INT type_null_pair;
+			#endregion
+
+
+			#region CONSTRUCTORS
+			//_______________________________________________________________________________________________________________________________________
+			public LI_EHS_Order( int value ) : base( value )
+			{
+			}
+			//_______________________________________________________________________________________________________________________________________
+			public LI_EHS_Order( NULL_INT tnp ) : base( tnp )
+			{
+				type_null_pair = tnp;
+			}
+			#endregion
+
+
+			#region METHODS
+			//_______________________________________________________________________________________________________________________________________
+			public FACTORS Factors
+			{
+				get { return column_factors; }
+			}
+			//_______________________________________________________________________________________________________________________________________
+			public int Ordinal
+			{
+				get { return Factors.Ordinal; }
+			}
+			//_______________________________________________________________________________________________________________________________________
+			override public string ToString()
+			{
+				return base.Value.ToString();
+			}
+			//___________________________________________________________________________________________________________________________________
+			/// <summary>
+			/// Returns value that is sent to the database.
+			/// </summary>
+			override public object DbWriteValue
+			{
+				get { return base.DbWriteValue; }
+			}
+			//___________________________________________________________________________________________________________________________________
+			/// <summary>
+			/// Returns the value that is displayed in a TextBox.
+			/// </summary>
+			override public string TextboxValue
+			{
+				get { return base.TextboxValue; }
+			}
+			//___________________________________________________________________________________________________________________________________
+			/// <summary>
+			/// Returns Notes as used in a VCF file.
+			/// </summary>
+			override public string VcfValue
+			{
+				get { return "base.AsTF"; }
+			}
+			//___________________________________________________________________________________________________________________________________________
+			/// <summary>
+			/// Returns true if person has a valid Notes value.
+			/// </summary>
+			override public bool IsVcfValue
+			{
+				get { return base.IsNotAbsoluteNull; }
+			}
+			#endregion
+
+
+			#region DB INTERFACE
+			//_______________________________________________________________________________________________________________________________________
+			override public OleDbParameter DbParameter
+			{
+				get
+				{
+					OleDbParameter parameter = base.DbParameter;
+					parameter.ParameterName = Factors.ParameterName;
+					parameter.Size = Factors.FieldWidth;
+					parameter.Value = this.DbWriteValue;
+					return parameter;
+				}
+			}
+			#endregion
+		}
+	}
+}
