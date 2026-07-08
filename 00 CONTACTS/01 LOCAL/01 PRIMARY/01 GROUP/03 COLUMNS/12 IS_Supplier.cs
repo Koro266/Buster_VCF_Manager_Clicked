325bc0d048842dccca1608e2cb588d2e9d4b677c
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-07-09 09:54:59 +1200

    Group: Add/Insert boolean columns.

---
+++ 00 CONTACTS/01 LOCAL/01 PRIMARY/01 GROUP/03 COLUMNS/12 IS_Supplier.cs
@@ -0,0 +1,76 @@
+﻿//___________________________________________________________________________________________________________________________________________________
+using System.Data.OleDb;
+//GLOBAL
+using BOOL		= CONTACTS.GLOBAL.DATABASE.COLUMN.True_False;
+using NULL_BOOL = CONTACTS.GLOBAL.DATABASE.COLUMN.TypeNullPair<bool>;
+using FACTORS	= CONTACTS.GLOBAL.TOOLS.ColumnFactors;
+//LOCAL
+using CONST		= CONTACTS.LOCAL.PRIMARY.PERSON.Constants;
+using ORDINAL	= CONTACTS.LOCAL.PRIMARY.PERSON.Constants.OrdinalByName;
+
+//___________________________________________________________________________________________________________________________________________________
+namespace CONTACTS.LOCAL.PRIMARY.PERSON
+{
+	//_______________________________________________________________________________________________________________________________________________
+	public partial class Column
+	{
+		//___________________________________________________________________________________________________________________________________________
+		public partial class IS_NoRightPerson : BOOL
+		{
+			#region DECLARATIONS
+			private static FACTORS column_factors = CONST.Factors[ORDINAL.NoRightPerson];
+			private NULL_BOOL type_null_pair;
+			#endregion
+
+
+			#region CONSTRUCTORS
+			//_______________________________________________________________________________________________________________________________________
+			public IS_NoRightPerson( bool value ) : base( value )
+			{
+			}
+			//_______________________________________________________________________________________________________________________________________
+			public IS_NoRightPerson( NULL_BOOL tnp ) : base( tnp )
+			{
+				type_null_pair = tnp;
+			}
+			#endregion
+
+
+			#region METHODS
+			//_______________________________________________________________________________________________________________________________________
+			public FACTORS Factors
+			{
+				get { return column_factors; }
+			}
+			//_______________________________________________________________________________________________________________________________________
+			public int Ordinal
+			{
+				get { return Factors.Ordinal; }
+			}
+			//___________________________________________________________________________________________________________________________________
+			/// <summary>
+			/// Returns NoRightPerson as used in a VCF file.
+			/// </summary>
+			override public string VcfValue
+			{
+				get { return base.AsTF; }
+			}
+			#endregion
+
+
+			#region DB INTERFACE
+			//_______________________________________________________________________________________________________________________________________
+			override public OleDbParameter DbParameter
+			{
+				get
+				{
+					OleDbParameter parameter = base.DbParameter;
+					parameter.ParameterName = Factors.ParameterName;
+					parameter.Size = Factors.FieldWidth;
+					return parameter;
+				}
+			}
+			#endregion
+		}
+	}
+}
