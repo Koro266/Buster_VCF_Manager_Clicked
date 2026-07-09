d1bad591a874e852048bdc376ab6ff1f6fe4d3d1
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-07-09 13:29:26 +1200

    Update Group.Row.FIELD getters.

--- 00 CONTACTS/01 LOCAL/01 PRIMARY/01 GROUP/02 ROW/00 Row, Columns (Group).cs
+++ 00 CONTACTS/01 LOCAL/01 PRIMARY/01 GROUP/02 ROW/00 Row, Columns (Group).cs
@@ -17,10 +17,20 @@ namespace CONTACTS.LOCAL.PRIMARY.GROUP
 		{ }
 
 		//_____________________________________________________________(_CASTING TYPE_)______________________________________________________________
-		public FIELD.PK_Group 			PkGroup			{ get { return ( FIELD.PK_Group )			base.GetField( ORDINAL.PkGroup );		} }
-		public FIELD.ST_GroupName 		GroupName		{ get { return ( FIELD.ST_GroupName )		base.GetField( ORDINAL.GroupName );		} }
-		public FIELD.ST_GroupType 		GroupType		{ get { return ( FIELD.ST_GroupType )		base.GetField( ORDINAL.GroupType );		} }
-		public FIELD.ST_Notes 			Notes			{ get { return ( FIELD.ST_Notes )			base.GetField( ORDINAL.Notes );			} }
-		public FIELD.DT_CurrencyDate 	CurrencyDate	{ get { return ( FIELD.DT_CurrencyDate )	base.GetField( ORDINAL.CurrencyDate );	} }
+		public FIELD.PK_Group 			PkGroup			{ get { return ( FIELD.PK_Group )			base.GetField( ORDINAL.PkGroup ); } }
+		public FIELD.ST_GroupName 		GroupName		{ get { return ( FIELD.ST_GroupName )		base.GetField( ORDINAL.GroupName ); } }
+		public FIELD.ST_GroupType 		GroupType		{ get { return ( FIELD.ST_GroupType )		base.GetField( ORDINAL.GroupType ); } }
+		public FIELD.ST_Notes 			Notes			{ get { return ( FIELD.ST_Notes )			base.GetField( ORDINAL.Notes ); } }
+		public FIELD.DT_CurrencyDate 	CurrencyDate	{ get { return ( FIELD.DT_CurrencyDate )	base.GetField( ORDINAL.CurrencyDate ); } }
+		public FIELD.IS_Selected 		Selected		{ get { return ( FIELD.IS_Selected )		base.GetField( ORDINAL.Selected ); } }
+		public FIELD.IS_DefaultRow 		DefaultRow		{ get { return ( FIELD.IS_DefaultRow )		base.GetField( ORDINAL.DefaultRow ); } }
+		public FIELD.IS_Export 			Export			{ get { return ( FIELD.IS_Export )			base.GetField( ORDINAL.Export ); } }
+		public FIELD.IS_Blocked 		Blocked			{ get { return ( FIELD.IS_Blocked )			base.GetField( ORDINAL.Blocked ); } }
+		public FIELD.IS_Inactive 		Inactive		{ get { return ( FIELD.IS_Inactive )		base.GetField( ORDINAL.Inactive ); } }
+		public FIELD.IS_StTheresa 		StTheresa		{ get { return ( FIELD.IS_StTheresa )		base.GetField( ORDINAL.StTheresa ); } }
+		public FIELD.IS_Tradesman 		Tradesman		{ get { return ( FIELD.IS_Tradesman )		base.GetField( ORDINAL.Tradesman ); } }
+		public FIELD.IS_Supplier 		Supplier		{ get { return ( FIELD.IS_Supplier )		base.GetField( ORDINAL.Supplier ); } }
+		public FIELD.IS_Writer 			Writer			{ get { return ( FIELD.IS_Writer )			base.GetField( ORDINAL.Writer ); } }
+
 	}
 }
