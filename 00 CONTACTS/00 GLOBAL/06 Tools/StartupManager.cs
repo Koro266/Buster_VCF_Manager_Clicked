048a6311b1ebd441511d345316296c18c240ba2e
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-07-14 20:18:40 +1200

    MASTER:Startup. Change startup form to Overseer.

--- 00 CONTACTS/00 GLOBAL/06 Tools/StartupManager.cs
+++ 00 CONTACTS/00 GLOBAL/06 Tools/StartupManager.cs
@@ -21,7 +21,7 @@ namespace CONTACTS.GLOBAL.TOOLS
 		};
 
 		#if DEBUG
-		private static StartupForm startup_Interface = StartupForm.Address;
+		private static StartupForm startup_Interface = StartupForm.Overseer;
 		#else
 		private static StartupForm startup_Interface = StartupForm.Overseer;
 		#endif
