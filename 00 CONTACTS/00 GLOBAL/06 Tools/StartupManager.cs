e29f8366c0597020d6a5c4e824c2be69bc46b64a
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-07-16 18:41:46 +1200

    Move the HEAD back to master

--- 00 CONTACTS/00 GLOBAL/06 Tools/StartupManager.cs
+++ 00 CONTACTS/00 GLOBAL/06 Tools/StartupManager.cs
@@ -20,7 +20,7 @@ namespace CONTACTS.GLOBAL.TOOLS
 		};
 
 		#if DEBUG
-		private static StartupForm startup_Interface = StartupForm.Overseer;
+		private static StartupForm startup_Interface = StartupForm.Device;
 		#else
 		private static StartupForm startup_Interface = StartupForm.Overseer;
 		#endif
