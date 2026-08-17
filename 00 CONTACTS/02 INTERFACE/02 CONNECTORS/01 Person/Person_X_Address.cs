2bd4eb216bb2eb681ff44afea811cffece3bb4bd
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-08-18 05:48:21 +1200

    PERSON_X_ADDRESS: Create form, otherwise unaltered..

---
+++ 00 CONTACTS/02 INTERFACE/02 CONNECTORS/01 Person/Person_X_Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VcfManager._00_CONTACTS._02_INTERFACE._02_CONNECTORS._01_Person
+{
+	public partial class Person_X_Address : Form
+	{
+		public Person_X_Address()
+		{
+			InitializeComponent();
+		}
+	}
+}
