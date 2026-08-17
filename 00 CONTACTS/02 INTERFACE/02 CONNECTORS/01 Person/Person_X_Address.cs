f5dc805a689392c80d85a545673734d6ffce5146
Brusster <Brusster@DESKTOP-7Q27I7C>
2026-08-18 06:05:42 +1200

    PERSON_X_ADDRESS: Set up the basic formatting of the form code file.

--- 00 CONTACTS/02 INTERFACE/02 CONNECTORS/01 Person/Person_X_Address.cs
+++ 00 CONTACTS/02 INTERFACE/02 CONNECTORS/01 Person/Person_X_Address.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿//___________________________________________________________________________________________________________________________________________________
+//GLOBAL
+using EVENT_STATE	= CONTACTS.GLOBAL.TOOLS.EventState;
+using GLOBAL_DB		= CONTACTS.GLOBAL.DATABASE.CONNECTION.DbConnector;
+using GLOBAL_PRESET	= CONTACTS.GLOBAL.VALUES.CONSTANT.Preset;
+using MESSENGER		= CONTACTS.GLOBAL.TOOLS.Messenger;
 
+//___________________________________________________________________________________________________________________________________________________
 namespace CONTACTS.INTERFACE.CONNECTORS
 {
+	//___________________________________________________________________________________________________________________________________________________
 	public partial class Person_X_Address : Form
 	{
+		//___________________________________________________________________________________________________________________________________________________
 		public Person_X_Address()
 		{
 			InitializeComponent();
